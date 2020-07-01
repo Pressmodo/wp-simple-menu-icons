c757e5723bb2a4e6bb51738bb0f4149077c33fab
Alessandro Tesoro <alessandro.tesoro@icloud.com>
2020-07-01 14:16:03 +0100

    load fa source files

--- webpack.mix.js
+++ webpack.mix.js
@@ -104,7 +104,9 @@ var sassConfig = {
 
 // Compile SASS/CSS.
 mix.sass( `${devPath}/scss/admin.scss`, 'css', sassConfig );
-mix.copy( 'node_modules/@fortawesome/fontawesome-free/metadata/icons.yml', 'dist/metadata')
+mix.copy( 'node_modules/@fortawesome/fontawesome-free/css/all.min.css', 'dist/css' )
+mix.copy( 'node_modules/@fortawesome/fontawesome-free/webfonts', 'dist/webfonts' )
+mix.copy( 'node_modules/@fortawesome/fontawesome-free/metadata/icons.yml', 'dist/metadata' )
 
 /*
  * Add custom Webpack configuration.
